d88f35ec371c6d3c6ac47e2bf76245246ee435d3
ratul544388 <ratulhossainbnc@gmail.com>
2024-04-01 16:45:05 +0600

    first commit

---
+++ src/components/chat-pluggin.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { Crisp } from "crisp-sdk-web";
+import { useEffect } from "react";
+export const ChatPluggin = () => {
+
+  useEffect(() => {
+    Crisp.configure("9ed0fbbc-ff1f-434b-b619-053d6534da47", {
+      autoload: false,
+    });
+    Crisp.chat.show();
+  }, []);
+
+  return (
+     <div>
+        ChatPluggin
+     </div>
+    );
+}
