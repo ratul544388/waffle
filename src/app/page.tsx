79078295b16f72cd4017973f11d01b23db0e3448
ratul544388 <ratulhossainbnc@gmail.com>
2024-04-01 16:50:08 +0600

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ChatPluggin } from "@/components/chat-pluggin";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center">
+    <main className="flex items-center justify-center min-h-screen">
       <h1 className="text-5xl font-bold text-center">Hello, Welcome to Our website</h1>
       <ChatPluggin/>
     </main>
--- src/components/chat-pluggin.tsx
+++ src/components/chat-pluggin.tsx
@@ -12,7 +12,7 @@ export const ChatPluggin = () => {
 
   return (
      <div>
-        ChatPluggin
+        
      </div>
     );
 }
